400e0d8ae567effa713885d1d26bb864111c4e86
Justyna Blazejewicz <jusblazejewicz@gmail.com>
2020-07-19 17:06:48 +0200

    js exercises

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-const name='Justyna';
-const age=23;
+const from='A';
+const to='Z';
+const name='Justyna'
+const age = '24'
 
+console.log(from);
+console.log(to);
+console.log(`Tu zaplanujesz swoje wesele od ${from} do ${to}!`);
 console.log(name);
 console.log(age);
-console.log(`Nazywam się ${name} i mam ${age} lata`);
+console.log(`Cześć, nazywam się ${name} i mam ${age} lata.`)
 
+const title = document.querySelector('.title');
+
+console.log (title);
+
+title.innerHTML=`Cześć, nazywam się ${name} i mam ${age} lata.<br> Ze mną zaplanujesz swoje wesele od ${from} do ${to}!`
