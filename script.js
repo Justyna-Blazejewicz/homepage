f1a75c13f698b688165be851a99ca6974efba803
Justyna Blazejewicz <jusblazejewicz@gmail.com>
2020-08-05 19:57:49 +0200

    JS exercises and bacground changes

--- script.js
+++ script.js
@@ -15,3 +15,28 @@ const title = document.querySelector('.title');
 console.log (title);
 
 title.innerHTML=`Cześć, nazywam się ${name} i mam ${age} lata.<br> Ze mną zaplanujesz swoje wesele od ${from} do ${to}!`
+
+function calculate(myNumber){
+    return myNumber*7;
+}
+const myResult=calculate(3)
+console.log(myResult)
+
+function greet(age, firstName){
+    console.log(
+        `Cześć! Mam na imię ${firstName} i mam ${age} lata`
+    )
+}
+greet(24, 'Justyna')
+
+const greet2 = (age2, firstName2) => {
+    console.log(
+        `Cześć! Mam na imię ${firstName2} i mam ${age2} lata`
+    )
+}
+greet2(24, 'Justyna')
+
+const calculate2 = (myNumber2) => (myNumber2+3)*7
+
+const myResult2=calculate2(2)
+console.log(myResult2)
